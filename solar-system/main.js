553e68f1db48265e0f7a465ca7cb50cd3318f556
cblack389 <negruclaudiu389@yahoo.com>
2022-05-18 17:16:08 +0300

    basic setup

---
+++ solar-system/main.js
@@ -0,0 +1,34 @@
+// import * as THREE from '/build/three.module.js'
+// import { OrbitControls } from './jsm/controls/OrbitControls.js'
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+
+import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+const controls = new OrbitControls( camera, renderer.domElement );
+			controls.target.set( 0, 0.5, 0 );
+			controls.update();
+			controls.enablePan = false;
+			controls.enableDamping = true;
+
+function animate() {
+	requestAnimationFrame( animate );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    controls.update();
+	renderer.render( scene, camera );
+}
+animate();
